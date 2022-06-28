461e0f1b05e10be5c70775efee117f2baf3967f8
William Lin <william.linn02@gmail.com>
2022-06-28 15:24:34 -0400

    updated redeploy.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-tmux kill-server
 cd "portfolio-site"
 git fetch && git reset origin/main --hard
 
-tmux new -d 'source python3-virtualenv/bin/activate && pip3 install -r requirements.txt && flask run --host=0.0.0.0'
+source python3-virtualenv/bin/activate
+pip3 install -r requirements.txt
+
+systemctl restart myportfolio
