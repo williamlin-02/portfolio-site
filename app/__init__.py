3d274ecc94f32ce2fc496fda5bf64e4fbb928667
William Lin <william.linn02@gmail.com>
2022-06-28 13:07:21 -0400

    adding curl-test.sh

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,32 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from peewee import *
+import datetime
+from playhouse.shortcuts import model_to_dict
 
 load_dotenv()
 app = Flask(__name__)
+
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+        user = os.getenv("MYSQL_USER"),
+        password = os.getenv("MYSQL_PASSWORD"),
+        host = os.getenv("MYSQL_HOST"),
+        port = 3306)
+
+print(mydb)
+
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = mydb
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
 
 '''
     dynamic navigation bar
@@ -30,6 +53,29 @@ def projects():
 @app.route('/map')
 def map():
     return render_template('map.html', title="Map", url=os.getenv("URL"))
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+    return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_posts': [
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
+
+# not working yet
+@app.route('/api/timeline_post', methods=['DELETE'])
+def delete_time_line_post():
+    model_to_dict(p)
+    for p in TimelinePost.select().delete()
 
 
 if __name__ == "__main__":
---
+++ curl-test.sh
@@ -0,0 +1,5 @@
+# curl --request POST http://localhost:5000/api/timeline_post -d 'name=William&email=williamlin@uchicago.edu&content=Testing my endpoints with postman and curl'
+
+# curl --request GET http://localhost:5000/api/timeline_post
+
+curl --request DELETE http://localhost:5000/api/timeline_post
