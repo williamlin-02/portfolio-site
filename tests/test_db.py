0d6cb5f4d8b1495a752b58c3c3b69a9e1d90d83c
amberxuemingxin <amberxuemingxin@gmail.com>
2022-07-04 18:54:44 -0400

    test_db and test_app

---
+++ tests/test_db.py
@@ -0,0 +1,41 @@
+import unittest
+from peewee import *
+from app import TimelinePost
+
+
+MODELS = [TimelinePost]
+
+# use an in-memory SQLite for tests.
+test_db = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
+
+class TestTimelinePost(unittest.TestCase):
+    def setUp(self):
+        # Bind model classes to test db. Since we have a complete list of
+        # all models, we do not need to recursively bind dependencies.
+        test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
+
+        test_db.connect()
+        test_db.create_tables(MODELS)
+
+    def tearDown(self):
+        # Not strictly necessary since SQLite in-memory databases only live
+        # for the duration of the connection, and in the next step we close
+        # the connection...but a good practice all the same.
+        test_db.drop_tables(MODELS)
+
+        # Close connection to db.
+        test_db.close()
+
+        # If we wanted, we could re-bind the models to their original
+        # database here. But for tests this is probably not necessary.
+
+        def test_timeline_post(self):
+            # Create 2 timeline posts
+            first_post = TimelinePost.create(name='John Doe', email='john@example.com', content='Hello world, I\'m John!')
+            assert first_post.id == 1
+            second_post = TimelinePost.create(name='Jane Doe', email='jane@example.com', content='Hello world, I\'m Jane!')
+            assert second_post.id == 2
+
+            # TODO: GET timeline posts and assert that they are correct
+            for i, post in enumerate(TimelinePost.select()):
+                assert post.id == i
