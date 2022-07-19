defaa92c8c17cfddd7a5f337f0eac3a75a14b408
williamlin-02 <103897960+williamlin-02@users.noreply.github.com>
2022-07-19 13:35:54 -0500

    Delete curl-test.sh

--- curl-test.sh
+++
@@ -1,5 +0,0 @@
-curl --request POST http://localhost:5000/api/timeline_post -d 'name=William&email=williamlin@uchicago.edu&content=Testing my endpoints with postman and curl'
-
-curl --request GET http://localhost:5000/api/timeline_post
-
-curl --request DELETE http://localhost:5000/api/timeline_post
